02265e9fc7186712005ab46c08ee42d61ab43bcd
Oyvind Moller Asbjornsen <oyvindma@gmail.com>
2014-06-19 22:00:43 +0200

    Added basic Application

---
+++ src/main/java/oyvindma/examples/dropwizard_helloworld/HelloWorldApplication.java
@@ -0,0 +1,28 @@
+package oyvindma.examples.dropwizard_helloworld;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+	public static void main(String[] args) throws Exception {
+		new HelloWorldApplication().run(args);
+	}
+
+	@Override
+	public String getName() {
+		return "hello-world";
+	}
+
+	@Override
+	public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+		//TODO
+	}
+
+}
