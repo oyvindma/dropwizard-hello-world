49bf41ea2d13edf00b24f5c5ed3927012a118459
Oyvind Moller Asbjornsen <oyvindma@gmail.com>
2014-06-19 22:01:35 +0200

    added HelloWorldResource and Saying added helloworld YAML config file sdded Configuration file for the YAML-file to be loaded into

---
+++ src/main/java/oyvindma/examples/dropwizard_helloworld/HelloWorldResource.java
@@ -0,0 +1,40 @@
+package oyvindma.examples.dropwizard_helloworld;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.codahale.metrics.annotation.Timed;
+import com.google.common.base.Optional;
+
+/*
+ * Warning
+ * Resource classes are used by multiple threads concurrently. 
+ * In general, we recommend that resources be stateless/immutable, 
+ * but it’s important to keep the context in mind.
+ */
+
+@Path("/hello-world")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloWorldResource {
+	private final String template;
+	private final String defaultName;
+	private final AtomicLong counter;
+
+	public HelloWorldResource(String template, String defaultName) {
+		this.template = template;
+		this.defaultName = defaultName;
+		this.counter = new AtomicLong();
+	}
+
+	@GET
+	@Timed
+	public Saying sayHello(@QueryParam("name") Optional<String> name) {
+		final String value = String.format(template, name.or(defaultName));
+		return new Saying(counter.incrementAndGet(), value);
+	}
+}
